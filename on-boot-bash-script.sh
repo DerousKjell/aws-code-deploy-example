a3b4d915f6f4c067d3bdcd045e02f9e959e4edac
DerousKjell <derous.kjell@live.be>
2020-10-04 15:15:21 +0200

    Add docs

---
+++ on-boot-bash-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+su
+yum -y update
+yum -y install ruby wget httpd 
+chkconfig httpd on  
+service httpd start
+cd /home/ec2-user
+wget https://aws-codedeploy-eu-west-3.s3.eu-west-3.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
+service codedeploy-agent status
